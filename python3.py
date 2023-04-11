0034e77372da8e4d2763555aca0102ad7d33953e
PatrykZdz <115219600+PatrykZdz@users.noreply.github.com>
2023-04-11 15:36:18 +0200

    Create python3.py

---
+++ python3.py
@@ -0,0 +1,64 @@
+#Ćwicznie 1
+class Fruit:
+    def __init__(self,color,weight):
+        self.color = color
+        self.weight = weight
+
+    def __str__(self):
+        return str("color "+str(self.color)+"\n weight:"+str(self.weight))
+
+    def __add__(self,other):
+        return Fruit(' ',self.weight+other.weight)
+
+class Apple(Fruit):
+    def __init__(self,color):
+        self.color = color
+    def isfresh(self):
+        if(self.color == "red" or self.color == "green"):
+            return True
+        else:
+            return False
+class Banan(Fruit):
+    def __init__(self,color):
+        self.color = color
+    def isfresh(self):
+        if(self.color == "yellow"):
+            return True
+        else:
+            return False
+class Orange(Fruit):
+    def __init__(self,color):
+        self.color = color
+    def isfresh(self):
+        if(self.color == "orange"):
+            return True
+        else:
+            return False
+
+my_fruit=Fruit("czerwony,",350)
+my_fruit2=Fruit("pomaranczowe",250)
+print(my_fruit+my_fruit2)
+
+
+#Ćwiczenie 2
+class Account:
+    def __init__(self,account_number,balance=0):
+        self.account_number = account_number
+        self.balance = balance
+    def deposit(self,amount):
+        self.balance += amount
+    def withdraw(self,amount):
+        if (amount > self.balance):
+            print("Brak srodkow")
+        else:
+            self.balance -= amount
+    def transfer(self,amount,to_account):
+        if amount > self.balance:
+            print("Brak srodkow")
+        else:
+            self.withdraw(amount)
+            to_account.deposit(amount)
+class PrivatAccount(Account):
+    def __init__(self,account_number,balance = 0):
+        self.account_number=account_number
+        self.balance=balance
