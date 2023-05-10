c31982d5484aa3fca4801677a93331935bcb6f5e
PatrykZdz <115219600+PatrykZdz@users.noreply.github.com>
2023-05-10 13:42:50 +0200

    Update Zadania z C2.c

--- Zadania z C2.c
+++ Zadania z C2.c
@@ -1,3 +1,86 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <wchar.h>
+#include <string.h>
+void zeruj2(int**t,unsigned int n, unsigned int m)
+{
+    int suma = 0;
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0; j<n;j++)
+        {
+            suma = suma+t[i][j];
+        }
+    }
+    return suma;
+}
+void wczytaj(int **t,unsigned int n,unsigned int m)
+{
+    for (int i= 0; i<n;i++)
+    {
+        for(int j = 0; j<m;j++)
+        {
+            scanf("%d",t[i][j]);
+        }
+    }
+}
+void wypisz2(unsigned int n, unsigned int m, int t[][m])
+{
+    for(int i = 0; i<n;i++)
+    {
+        for (int j = 0; j<m;j++)
+        {
+            printf("%d\t",t[i][j]);
+        }
+    }
+    printf("\n");
+}
+
+void zeruj3(unsigned int n,unsigned int m, int t[][m])
+{
+    for(int i = 0;i<n;i++)
+    {
+        for(int j = 0; j<n;j++)
+        {
+            t[i][j]=0;
+        }
+    }
+}
+int** alokuj(unsigned int n, unsigned int m)
+{
+    int **t=malloc(n*sizeof(int*));
+    for(int i= 0; i<n;i++)
+    {
+        t[i]=malloc(m*sizeof(int));
+    }
+    return t;
+}
+void wypisz(int **t,unsigned int n, unsigned int m)
+{
+    for(int i = 0; i <n;i++)
+    {
+        for(int j = 0;j<m;j++)
+        {
+            printf("%d\t",t[i][j]);
+        }
+    printf("\n");
+    }
+}
+
+int main()
+{
+    int **t=alokuj(2,2);
+    wczytaj(t,2,2);
+    wypisz(t,2,2);
+
+
+
+
+
+    return 0;
+}
+
+
 Zadanie 5.2.9
 #include <stdio.h>
 void wytnij(char* napis, int n, int m)
