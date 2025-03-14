e31618b9273e3fe85f08d1ace0f056882706eaff
PatrykZdz <115219600+PatrykZdz@users.noreply.github.com>
2025-03-14 09:22:22 +0100

    Create trip.py

---
+++ pliki/trip.py
@@ -0,0 +1,18 @@
+class Trip:
+    def __init__(self,destination,duration):
+        self.destination = destination
+        self.duration = duration
+        self.participants = []
+
+    def calculate_cost(self):
+        return self.duration * 100
+
+    def add_participant(self,participant):
+       if participant == "":
+           raise ValueError("Participant name cannot be empty")
+
+       self.participants.append(participant)
+
+
+
+
